704d1d44c83d74fa6b1bca5000de841a97db931a
Kitsana <folklove45@gmail.com>
2016-04-06 03:02:06 +0700

    NewProject

--- MonkeyPinch/ViewController.swift
+++ MonkeyPinch/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translationInView(self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x:view.center.x + translation.x,
+                y:view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPointZero, inView: self.view)
+    }
 }
 
